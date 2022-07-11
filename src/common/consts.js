6aa481ff5ddd49288c9f44f2167064144c0eef7d
kapitan <62106615+irklka@users.noreply.github.com>
2022-07-11 22:50:47 +0400

    added dfs/bfs and project description

--- src/common/consts.js
+++ src/common/consts.js
@@ -29,6 +29,13 @@ const selectionSortOverView =
 
 const selectionSortWorstComplexity =
   'საუკეთესო, საშუალო და უარესი შემთხვევა: n2 (მონაცემთა მიმდევრობა არ ახდენს გავლენას ალგორითმის მუშაობის სიჩქარეზე).';
+  const dfsOverView =
+  'Depth-First Search ან DFS ალგორითმი იკვლევს მონაცემთა სტრუქტურებს, როგორიცაა ხეები და გრაფიკები. იგი არის რეკურსიული ალგორითმი, რომელიც იყენებს backtracking-ის პრინციპს. ის გულისხმობს ყველა კვანძის ამომწურავი ძიების ჩატარებას, თუ ეს შესაძლებელია, წინსვლას და საჭიროების შემთხვევაში უკან დაბრუნებას.';
+  const bfsOverView =
+  'Breadth-First Search ძიება ან BFS ალგორითმი გამოიყენება ხის ან გრაფის გამოსაკვლევად, ისეთი კვანძისთვის, რომელიც აკმაყოფილებს კრიტერიუმების ერთობლიობას. ის იწყება ხის ან გრაფის ფესვიდან და იკვლევს ყველა კვანძს მიმდინარე სიღრმეზე, სანამ გადავა კვანძებზე მომდევნო დონეზე. თქვენ შეგიძლიათ გადაჭრათ მრავალი პრობლემა გრაფის თეორიაში BFS-ის საშუალებით.';
+
+ export const aboutProjectText = 
+  "AlgoAIO წარმოადგენს ინსტრუმენტს სტუდენტებისთვის და არამარტო, გაიმარტივონ ალგორითმების შესწავლა ვიზუალიზაციის მხრივ. თქვენ შეგიძლიათ აირჩიოთ სასურველი ალგორითმი და დაიწყოთ მისი შესწავლა.";
 export const loremIpsumText =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae\n' +
   '        elementum dolor. Nulla auctor mattis nibh eu consectetur. Vivamus quis\n' +
@@ -112,8 +119,8 @@ export const learningMaterial = {
     },
   },
   'GRAPH-TRAVERSAL': {
-    'depth-first-search': {
-      fancyName: 'Depth first search',
+    'tree-traversals': {
+      fancyName: 'Tree Traversals',
       overview: bubbleSortOverView,
       complexities: {
         worst: bubbleSortWorstComplexity,
@@ -123,20 +130,124 @@ export const learningMaterial = {
       additionalSources: [
         'https://www.geeksforgeeks.org/bubble-sort/',
         'https://www.programiz.com/dsa/bubble-sort',
+      ],
+    },
+    'depth-first-search': {
+      fancyName: 'Depth first search',
+      overview: dfsOverView,
+      complexities: {},
+      additionalSources: [
+        'https://www.geeksforgeeks.org/depth-first-search-or-dfs-for-a-graph/',
+        'https://www.simplilearn.com/tutorials/data-structure-tutorial/dfs-algorithm',
       ],
     },
     'breadth-first-search': {
       fancyName: 'Breadth first search',
-      overview: bubbleSortOverView,
-      complexities: {
-        worst: bubbleSortWorstComplexity,
-        average: bubbleSortWorstComplexity,
-        best: bubbleSortWorstComplexity,
-      },
+      overview: bfsOverView,
+      complexities: {},
       additionalSources: [
-        'https://www.geeksforgeeks.org/bubble-sort/',
-        'https://www.programiz.com/dsa/bubble-sort',
+        'https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/?ref=lbp',
+        'https://www.simplilearn.com/tutorials/data-structure-tutorial/bfs-algorithm',
       ],
     },
   },
 };
+export const quiz =  {
+  "quizTitle": "React Quiz Component Demo",
+  "quizSynopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim",
+  "nrOfQuestions": "4",
+  "questions": [
+    {
+      "question": "How can you access the state of a component from inside of a member function?",
+      "questionType": "text",
+      "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
+      "answerSelectionType": "single",
+      "answers": [
+        "this.getState()",
+        "this.prototype.stateValue",
+        "this.state",
+        "this.values"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "ReactJS is developed by _____?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Google Engineers",
+        "Facebook Engineers"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "ReactJS is an MVC based framework?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "True",
+        "False"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "10"
+    },
+    {
+      "question": "Which of the following concepts is/are key to ReactJS?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Component-oriented design",
+        "Event delegation model",
+        "Both of the above",
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "30"
+    },
+    {
+      "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+      "questionType": "photo",
+      "answerSelectionType": "single",
+      "answers": [
+        "https://dummyimage.com/600x400/000/fff&text=A",
+        "https://dummyimage.com/600x400/000/fff&text=B",
+        "https://dummyimage.com/600x400/000/fff&text=C",
+        "https://dummyimage.com/600x400/000/fff&text=D"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "What are the advantages of React JS?",
+      "questionType": "text",
+      "answerSelectionType": "multiple",
+      "answers": [
+        "React can be used on client and as well as server side too",
+        "Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps",
+        "React components have lifecycle events that fall into State/Property Updates",
+        "React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer"
+      ],
+      "correctAnswer": [1, 2, 4],
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+  ]
+} 
--- src/components/InfoAboutAlgorithmPage/Quiz.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Quiz = () => {
-  return <div>Here goes Quiz</div>;
-};
-
-export default Quiz;
